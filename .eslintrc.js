0d3bda9884a6e6c91e3d50b9be32c5df4b49da53
mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>
2022-03-16 18:48:52 +0300

    maps data seed
    Signed-off-by: mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
+    quotes: ['error', 'double'],
     semi: ['error', 'always'],
     'no-console': 0,
     'require-atomic-updates': 0,
